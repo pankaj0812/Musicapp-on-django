98ed6530e824b678c65eca47e4e1ea361da33c0e
pankaj0812 <singhpankajkumar65@gmail.com>
2017-12-13 19:29:55 +0530

    database and template integrated

---
+++ music/views.py
@@ -0,0 +1,13 @@
+from django.http import Http404
+from django.shortcuts import render
+from .models import Album
+def index(request):
+    all_albums= Album.objects.all()
+    return render(request,'music/index.html',{'all_albums':all_albums})
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request,'music/detail.html',{'album':album})
